15cbf4001c0b491939dd3547f84f4ca7b533796a
Yfuod <106873959+Yfuod@users.noreply.github.com>
2025-11-16 00:01:38 +0800

    Delete src directory

--- src/commands/utility/ping.js
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-export default {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Ping test"),
-  async execute(i) {
-    i.reply("🏓 Pong! Sakura OK 🌸");
-  }
-};

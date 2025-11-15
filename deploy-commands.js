a15b308f68f4673c399251c6b3dc2bd7c67b0f88
Yfuod <106873959+Yfuod@users.noreply.github.com>
2025-11-16 05:18:19 +0800

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { REST, Routes } from "discord.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const cmds = [];
+
+const commandsPath = path.join(__dirname, "src/commands");
+for (const folder of fs.readdirSync(commandsPath)) {
+  const folderPath = path.join(commandsPath, folder);
+  for (const file of fs.readdirSync(folderPath).filter(f => f.endsWith(".js"))) {
+    const cmd = (await import(`file://${folderPath}/${file}`)).default;
+    cmds.push(cmd.data.toJSON());
+  }
+}
+
+const rest = new REST().setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: cmds })
+  .then(() => console.log("Commands deployed"))
+  .catch(console.error);
---
+++ index.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Client, GatewayIntentBits, Collection } from "discord.js";
+import config from "./config.json" assert { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "src/commands");
+if (fs.existsSync(commandsPath)) {
+  for (const folder of fs.readdirSync(commandsPath)) {
+    const folderPath = path.join(commandsPath, folder);
+    for (const file of fs.readdirSync(folderPath).filter(f => f.endsWith(".js"))) {
+      const cmd = (await import(`file://${folderPath}/${file}`)).default;
+      if (!cmd || !cmd.data) continue;
+      client.commands.set(cmd.data.name, cmd);
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, "src/events");
+if (fs.existsSync(eventsPath)) {
+  for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith(".js"))) {
+    const evt = (await import(`file://${eventsPath}/${file}`)).default;
+    if (evt.once) client.once(evt.name, (...a) => evt.execute(...a, client));
+    else client.on(evt.name, (...a) => evt.execute(...a, client));
+  }
+}
+
+client.login(process.env.TOKEN);

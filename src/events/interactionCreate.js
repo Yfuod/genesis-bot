350a353ead989e3dba3321b669811de98f46bfd3
Yfuod <106873959+Yfuod@users.noreply.github.com>
2025-11-16 02:35:36 +0800

    Implement interactionCreate event handler

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,1 +1,28 @@
-// TODO: This command will be generated fully later.
+export default {
+  name: "interactionCreate",
+  async execute(interaction, client) {
+    if (interaction.isChatInputCommand()) {
+      const cmd = client.commands.get(interaction.commandName);
+      if (!cmd)
+        return interaction.reply({
+          content: "Ara~ Command seems missing…",
+          ephemeral: true,
+        });
+
+      try {
+        await cmd.execute(interaction, client);
+      } catch (err) {
+        console.error(err);
+        return interaction.reply({
+          content: "Ara ara~ Something went wrong executing that command.",
+          ephemeral: true,
+        });
+      }
+    }
+
+    // For buttons / menus (future panel)
+    if (interaction.isButton()) {
+      // panel buttons will be added later
+    }
+  },
+};

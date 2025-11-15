4555c558a1270f9834abc5924408e31435e6e0ee
Yfuod <106873959+Yfuod@users.noreply.github.com>
2025-11-15 23:51:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 import { Client, GatewayIntentBits, Collection } from "discord.js";
+
+// Required for ESM to get __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const client = new Client({
   intents: [
@@ -12,9 +16,11 @@ const client = new Client({
   ]
 });
 
+// Command collection
 client.commands = new Collection();
 
-const commandsPath = path.join(process.cwd(), "src/commands");
+// Load commands
+const commandsPath = path.join(__dirname, "src/commands");
 for (const folder of fs.readdirSync(commandsPath)) {
   const folderPath = path.join(commandsPath, folder);
   for (const file of fs.readdirSync(folderPath).filter(f => f.endsWith(".js"))) {
@@ -23,11 +29,13 @@ for (const folder of fs.readdirSync(commandsPath)) {
   }
 }
 
-const eventsPath = path.join(process.cwd(), "src/events");
+// Load events
+const eventsPath = path.join(__dirname, "src/events");
 for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith(".js"))) {
   const evt = (await import(`file://${eventsPath}/${file}`)).default;
-  if (evt.once) client.once(evt.name, (...a) => evt.execute(...a, client));
-  else client.on(evt.name, (...a) => evt.execute(...a, client));
+  if (evt.once) client.once(evt.name, (...args) => evt.execute(...args, client));
+  else client.on(evt.name, (...args) => evt.execute(...args, client));
 }
 
+// LOGIN USING RAILWAY VARIABLE ONLY
 client.login(process.env.TOKEN);

64a915e0b970e46235945ca787336ccbf137c560
Yfuod <106873959+Yfuod@users.noreply.github.com>
2025-11-23 01:20:44 +0800

    Delete events directory

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-
-module.exports={
-  name:"guildMemberAdd",
-  async execute(member){
-    const r=member.guild.roles.cache.find(x=>x.name==="Member");
-    if(r) member.roles.add(r).catch(()=>{});
-  }
-}
